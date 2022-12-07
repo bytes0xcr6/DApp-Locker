de8999eecf8818b0c766c80ced7bc327bda8be37
CristianRicharte6 <cristianricharte6@gmail.com>
2022-12-07 10:43:29 +0100

    .

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 const submit = document.querySelector(".button-lock");
+const asset = document.querySelector("#asset");
+const lockPeriod = document.querySelector("#lockPeriod");
+const amount = document.querySelector("#amount");
 
 submit.addEventListener("click", () => {
-  console.log("Clicked");
+  if (!lockPeriod.value || !amount.value) {
+    window.alert("All the values are required!");
+  }
+  console.log(asset.value, lockPeriod.value, amount.value);
 });
